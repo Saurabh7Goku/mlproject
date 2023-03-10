a94caca0bba8e6b6bfec90c04c8444c26d462020
Saurabh7Goku <saurabhgk7@gmail.com>
2023-03-10 13:05:58 +0530

    Created Train Test Split/Data Ingestion

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,55 @@
+import os
+import sys 
+import pandas as pd
+from src.logger import logging
+from src.exception import CustomException
+
+
+
+from sklearn.model_selection import train_test_split 
+
+from dataclasses import dataclass
+
+
+@dataclass # this will help in craeating variable inside class without using init.. 
+class DataIngestionConfig:
+    train_data_path: str = os.path.join('artifacts', 'train.csv')
+    test_data_path: str = os.path.join('artifacts', 'test.csv')
+    raw_data_path: str = os.path.join('artifacts', 'data.csv')
+
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        
+        logging.info('Entered the data ingestion component')
+        
+        try:
+            df = pd.read_csv('notebooks\data\data.csv')
+            logging.info('Imported the data in the dataframe named as "df"')
+            
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+            logging.info('Train_test_split initiated')
+            
+            train_set, test_set = train_test_split(df, test_size = 0.2, random_state = 101)
+            
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+
+            test_set.to_csv(self.ingestion_config.test_data_path, index = False, header = True)
+
+            logging.info('Ingestion Has been Done...')
+
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+        except Exception as e:
+            raise CustomException(e, sys)
+
+if __name__=="__main__":
+    obj = DataIngestion()
+    obj.initiate_data_ingestion()
